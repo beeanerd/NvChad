6164c0ef183a19c3e24e939a0db74340f1dc320a
Alex <alex3whitlock@gmail.com>
2023-10-16 17:26:44 -0400

    modified mason table to have own file

---
+++ lua/custom/configs/conform.lua
@@ -0,0 +1,19 @@
+require("conform").setup({
+  formatters_by_ft = {
+    go = { "gofumpt", {"golines"}, {"goimports-reviser", "goimports"} },
+    lua = { "stylua" },
+    python = { "isort", "black" },
+    javascript = { { "prettierd", "prettier" } },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+  log_level = vim.log.levels.ERROR,
+  notify_on_error = true,
+})
+
+local opts = {
+
+}
+return opts
---
+++ lua/custom/configs/mason.lua
@@ -0,0 +1,14 @@
+local opts = {
+  ensure_installed = {
+    "gopls",
+    "gofumpt",
+    "goimports",
+    "goimports-reviser",
+    "golines",
+    "gomodifytags",
+    "gopls",
+    "gotests",
+    "lua-language-server",
+  }
+}
+return opts
