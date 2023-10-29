eae93e8cf42a52880b1e3e2761ba3d9c3a49eb0f
Alex Whitlock <ae3whitlock@gmail.com>
2023-10-29 13:27:36 -0400

    mason defaults

--- lua/custom/configs/mason.lua
+++ lua/custom/configs/mason.lua
@@ -9,6 +9,20 @@ local opts = {
     "gopls",
     "gotests",
     "lua-language-server",
+    "clang-format",
+    "clangd",
+    "python-lsp-server",
+    "black",
+    "angular-language-server",
+    "eslint_d",
+    "json-lsp",
+    "marksman",
+    "prettier",
+    "python-lsp-server",
+    "texlab",
+    "typescript-language-server",
+    "vale",
+    "zls",
   }
 }
 return opts
