4f1cfd1a808219bb174037aba69c2b6b2dd630da
Alex <alex3whitlock@gmail.com>
2023-11-10 23:05:28 -0500

    lecture notes

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,60 +1,80 @@
 local plugins = {
-  {
-    "williamboman/mason.nvim",
-    opts = function()
-      return require("custom.configs.mason")
-    end,
-  },
-  {
-    "ThePrimeagen/harpoon",
-  },
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    lazy = false,
-    config = function()
-      require "custom.configs.conform"
-    end,
-  },
-  {
-    "lervag/vimtex",
-    ft = "tex",
-    config = function()
-      require "custom.configs.vimtex"
-    end
-  },
-  {
-    "mfussenegger/nvim-dap",
-  },
-  {
-    "leoluz/nvim-dap-go",
-    ft = "go",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function(_, opts)
-      require("dap-go").setup(opts)
-      require("core.utils").load_mappings("dap_go")
-    end,
-  },
-  {
-    "olexsmir/gopher.nvim",
-    ft = "go",
-    config = function(_, opts)
-      require("gopher").setup(opts)
-      require("core.utils").load_mappings("gopher")
-    end,
-    build = function()
-      vim.cmd [[silent! GoInstallDeps]]
-    end,
-  },
+    {
+        "williamboman/mason.nvim",
+        opts = function()
+            return require("custom.configs.mason")
+        end,
+    },
+    {
+        "ThePrimeagen/harpoon",
+    },
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("neorg").setup {
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                        },
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+        lazy = false,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        lazy = false,
+        config = function()
+            require "custom.configs.conform"
+        end,
+    },
+    {
+        "lervag/vimtex",
+        ft = "tex",
+        config = function()
+            require "custom.configs.vimtex"
+        end
+    },
+    {
+        "mfussenegger/nvim-dap",
+    },
+    {
+        "leoluz/nvim-dap-go",
+        ft = "go",
+        dependencies = "mfussenegger/nvim-dap",
+        config = function(_, opts)
+            require("dap-go").setup(opts)
+            require("core.utils").load_mappings("dap_go")
+        end,
+    },
+    {
+        "olexsmir/gopher.nvim",
+        ft = "go",
+        config = function(_, opts)
+            require("gopher").setup(opts)
+            require("core.utils").load_mappings("gopher")
+        end,
+        build = function()
+            vim.cmd [[silent! GoInstallDeps]]
+        end,
+    },
 }
 return plugins
